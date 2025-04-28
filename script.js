12a10ad5c65dfe34659f13f7de4385dddb1246d3
Caio Henrique Braga Teixeira <caiohbragateixeira@gmail.com>
2025-04-27 21:28:40 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+idade = prompt("Informe a sua idade: ");
+if (idade < 18) {
+    alert("Você não tem permissão para jogar");
+} else {
+    alert("Vamos jogar!")
+}
+
+if (idade >= 18) {
+    escolhaplayer = prompt("Digite 1 para Pedra, 2 para Papel ou 3 para Tesoura");
+    if (escolhaplayer == 1) {
+        alert("Você escolheu Pedra");
+    }
+    if (escolhaplayer == 2) {
+        alert("Você escolheu Papel");
+    }
+    if (escolhaplayer == 3) {
+        alert("Você escolheu Tesoura");
+    }
+    escolhaCPU = Math.floor(Math.random() * 3) + 1;
+
+    //Player pedra, CPU pedra == empate
+    //player papel, CPU papel == empate
+    //player tesoura, CPU tesoura == empate
+
+    if (escolhaplayer == escolhaCPU) {
+        alert("Empate!");
+    }
+
+    //player pedra, CPU tesoura == player ganha
+    //player papel, CPU pedra == player ganha
+    //player tesoura, CPU papel == player ganha
+
+    if (escolhaplayer == 1 && escolhaCPU == 3) {
+        alert("Você ganhou! Computador escolheu " + escolhaCPU + "-Tesoura");
+    }
+    if (escolhaplayer == 2 && escolhaCPU == 1) {
+        alert("Você ganhou! Computador escolheu " + escolhaCPU + "-Pedra");
+    }
+    if (escolhaplayer == 3 && escolhaCPU == 2) {
+        alert("Você ganhou! Computador escolheu " + escolhaCPU + "-Papel");
+    }
+
+    //CPU pedra, player tesoura == CPU ganha
+    //CPU papel, player pedra == CPU ganha
+    //CPU tesoura, player papel == CPU ganha
+
+    if (escolhaplayer == 3 && escolhaCPU == 1) {
+        alert("Computador ganhou! Escolheu " + escolhaCPU + "-Pedra");
+    }
+    if (escolhaplayer == 1 && escolhaCPU == 2) {
+        alert("Computador ganhou! Escolheu " + escolhaCPU + "-Papel");
+    }
+    if (escolhaplayer == 2 && escolhaCPU == 3) {
+        alert("Computador ganhou! Escolheu " + escolhaCPU + "-Tesoura");
+    }
+}
